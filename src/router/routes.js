15fdec7f6c47c7bbb79fd958d4602e2ecd3b8cc8
christianlsb <chrislsb09@gmail.com>
2023-03-21 08:31:20 -0300

    refactor: structure initial refactored

---
+++ src/router/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+import { Home } from '../containers/';
+
+export default function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
