39847d9ad94d63a31f2e69ceebfa9512756a2b96
christianlsb <chrislsb09@gmail.com>
2023-03-22 12:49:49 -0300

    feat: created cart

---
+++ src/containers/Cart/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Header } from '../../components';
+import * as S from './styles';
+
+export const Cart = () => {
+  return (
+    <>
+      <Header />
+      <S.Container>
+        <h1>Cart</h1>
+      </S.Container>
+    </>
+  );
+};
